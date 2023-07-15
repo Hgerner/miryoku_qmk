c47fb010db7086d9d3925c78f4bebb21e573dc3a
hakan <h.gerner94@gmail.com>
2023-07-15 14:59:48 +0200

    Adds support for dactyl manuform with horizontally soldered diodes

---
+++ keyboards/handwired/dactyl_manuform/skeletyl_3x5_3/skeletyl_3x5_3.h
@@ -0,0 +1,56 @@
+/*
+Copyright 2021 @dlford
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "quantum.h"
+
+#define XXX KC_NO
+
+#define LAYOUT_split_3x5_3( \
+    L00, L01, L02, L03, L04,     R04, R03, R02, R01, R00, \
+    L10, L11, L12, L13, L14,     R14, R13, R12, R11, R10, \
+    L20, L21, L22, L23, L24,     R24, R23, R22, R21, R20, \
+              L32, L33, L34,     R34, R33, R32 \
+) { \
+    { L00, L01, L02, L03, L04 }, \
+    { L10, L11, L12, L13, L14 }, \
+    { L20, L21, L22, L23, L24 }, \
+    { XXX, XXX, L32, L33, L34 }, \
+    { R00, R01, R02, R03, R04 }, \
+    { R10, R11, R12, R13, R14 }, \
+    { R20, R21, R22, R23, R24 }, \
+    { XXX, XXX, R32, R33, R34 } \
+}	
+
+#define LAYOUT_split_3x6_3( \
+    L00, L01, L02, L03, L04, L05,     R05, R04, R03, R02, R01, R00, \
+    L10, L11, L12, L13, L14, L15,     R15, R14, R13, R12, R11, R10, \
+    L20, L21, L22, L23, L24, L25,     R25, R24, R23, R22, R21, R20, \
+                   L33, L34, L35,     R35, R34, R33 \
+) { \
+    { L00, L01, L02, L03, L04, L05 }, \
+    { L10, L11, L12, L13, L14, L15 }, \
+    { L20, L21, L22, L23, L24, L25 }, \
+    { XXX, XXX, XXX, L33, L34, L35 }, \
+    { R00, R01, R02, R03, R04, R05 }, \
+    { R10, R11, R12, R13, R14, R15 }, \
+    { R20, R21, R22, R23, R24, R25 }, \
+    { XXX, XXX, XXX, R33, R34, R35 } \
+}
+
+#define LAYOUT LAYOUT_split_3x6_3
